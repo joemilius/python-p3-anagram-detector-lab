1fc4cb0caec4ded170ae2450bd2470bf39b1a065
Joe Milius <joemilius@gmail.com>
2022-10-26 23:35:08 -0600

    completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        pass
+
+    def match(self, list):
+        found_list = []
+        for word in list:
+            if sorted([letter for letter in word]) == sorted([letter for letter in self.word]):
+                found_list.append(word)
+        
+        return found_list
+        pass
+    
+    
+    
+    
+    
+    pass
